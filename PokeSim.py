29c73f71d88b35b501ab871d636f3cba542b17c3
Ben Andken <ben.andken@gmail.com>
2015-02-28 09:18:20 -0800

    keep drawing until playable hand

--- PokeSim.py
+++ PokeSim.py
@@ -6,20 +6,29 @@ from random import shuffle
 with open('WaterDeck.csv', 'rb') as csvfile:
     cards = csv.reader(csvfile, delimiter=',')
     deck = list()
+
+
+    print "=========DECK==========="
     for card in cards:
         deck.append(card)        
 
-    shuffle(deck)
-    for card in deck:
-        print ', '.join(card)
+    playable_hand = False
 
-    hand = list()
-    for i in range(0,7):
-        hand.append(deck[i])
+    while not playable_hand:
+        shuffle(deck)
+        for card in deck:
+            print ', '.join(card)
 
-    print "========HAND==========="
-    for card in hand:
-        print ', '.join(card)
+        hand = list()
+        for i in range(0,7):
+            hand.append(deck[i])
+
+        print "=========HAND==========="
+        for card in hand:
+            print ', '.join(card)
         
-    # the second number is whether it's a basic pokemon to see if a mulligan is needed
-    
+            # the second number is whether it's a basic pokemon to see if a mulligan is needed
+            playable_hand = playable_hand or (card[2] == "1")
+
+        print "======Playable Hand: " + str(playable_hand)
+
