ed075c4807eee682fcdf7a9432352fdd83cc3e58
Ben Andken <ben.andken@gmail.com>
2015-03-01 10:57:02 -0800

    helper functions

--- PokeSim.py
+++ PokeSim.py
@@ -3,26 +3,36 @@
 import csv
 from random import shuffle
 
-
-def ContainsBasic(hand):
-    playable_hand = False
-    for card in hand:
-        # the second number is whether it's a basic pokemon to see if a mulligan is needed
-        playable_hand = playable_hand or (card[2] == "Basic")
-    return playable_hand
-    
+def ContainsType(cards, type):
+    for card in cards:
+        if(card[2] == type):
+            return True
+    return False
 
 def PlayBasic(hand, bench):
     basic = next(card for card in hand if card[2] == "Basic")
     bench.append(basic)
     hand.remove(basic)
 
+def DrawCard(hand, deck):
+    hand.append(deck[0])
+    del deck[0]
 
+def PrintCards(label, cards):
+    print label
+    for card in cards:
+        print card[0]
 
+def ContainsName(cards, name):
+    for card in cards:
+        if(card[0] == name):
+            return True
+    return False
 
-
-
-
+def BlastioseFirstTurn(hand, discard, deck):
+    if(len(hand) == 1):
+        return ContainsName(hand, "Archie's Ace in the Hole") and ContainsName(discard, "Blastoise")
+    return False
 
 sims = 1
 
@@ -41,50 +51,40 @@ for j in range(0,sims):
         playable_hand = False
 
         while not playable_hand:
-            print "========================"
-            print "=========DECK==========="
-            print "========================"
             deck = list()
             for card in cards:
                 deck.append(card)        
 
             #shuffle(deck)
-            for card in deck:
-                print ', '.join(card)
+            PrintCards("============\n=== DECK ===\n============", deck)
+    
+            for i in range(0,7):
+                DrawCard(hand, deck)
 
-            hand = list()
-            hand.extend(deck[:7])
-            del deck[:7]
-
-            playable_hand = ContainsBasic(hand)
+            playable_hand = ContainsType(hand, "Basic")
 
             mulligans = mulligans + int(not playable_hand)
-            print "======Playable Hand: " + str(playable_hand)
-
 
         if(mulligans in mulligans_by_number):
             mulligans_by_number[mulligans] = mulligans_by_number[mulligans] + 1
         else:
             mulligans_by_number[mulligans] = 1
 
-        print "====Actual Hand==="
-        for card in hand:
-            print card[0]
+        PrintCards("====Actual Hand====", hand)
 
 
         # prize cards
         prizes = list()
-        prizes.extend(deck[:6])
-        del deck[:6]
+        for i in range(0,6):
+            DrawCard(prizes, deck)
 
-        print "====Prizes==="
+        PrintCards("====Prizes===", prizes)
         disaster = False
         for card in prizes:
             # each card has a value...if all card of that value are in the prizes, that's a disaster
             deck_indices = [i for i, x in enumerate(deck) if x[1] == card[1]]
             prize_indices = [i for i, x in enumerate(prizes) if x[1] == card[1]]
 
-            print card[0]
             disaster = disaster | (len(deck_indices) == len(prize_indices))
 
         print "====Disaster: " + str(disaster)
@@ -93,22 +93,14 @@ for j in range(0,sims):
         bench = []
 
         # play a Basic and draw a card
-        if(ContainsBasic(hand)):
+        if(ContainsType(hand, "Basic")):
             PlayBasic(hand, bench)
 
+        DrawCard(hand, deck)
 
-        print "==========================New hand="
-        for card in hand:
-            print card[0]
-
-        print "==========================Bench="
-        for card in bench:
-            print card[0]
-
-        print "==========================Deck="
-        for card in deck:
-            print card[0]
-
+        PrintCards("==========================New hand=", hand)
+        PrintCards("==========================Bench=", bench)
+        PrintCards("==========================Deck=", deck)
 
 
 
