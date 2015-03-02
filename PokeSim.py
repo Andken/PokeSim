c1a21e6f33c07a93e0a862f1a88c649ad24e73a7
Ben Andken <ben.andken@gmail.com>
2015-03-01 23:51:04 -0800

    working PokeSim '

--- PokeSim.py
+++ PokeSim.py
@@ -4,78 +4,82 @@ import csv
 from random import shuffle
 import DeckOperations as do
 import BlastoiseFirstTurn as bft
+from copy import deepcopy
 
 sims = 1
 
+gold_deck = list()
 mulligans_by_number = {}
 mulligans_by_number[0] = 0
 disasters = 0
 successes = 0
 memoized = {}
 
+with open('WaterDeck.csv', 'rb') as csvfile:
+    cards = csv.reader(csvfile, delimiter=',')
+    for card in cards:
+        gold_deck.append(tuple(card))        
+
+
 for j in range(0,sims):
-    with open('WaterDeck.csv', 'rb') as csvfile:
-        cards = csv.reader(csvfile, delimiter=',')
-        deck = list()
-        discard = list()
+    hand = list()
+    discard = list()
+
+    mulligans = 0
+    playable_hand = False
+
+    while not playable_hand:
+        deck = deepcopy(gold_deck)
+
+        shuffle(deck)
+        do.PrintCards("============\n=== DECK ===\n============", deck)
 
         hand = list()
-        mulligans = 0
-        playable_hand = False
+        for i in range(0,7):
+            do.DrawCard(hand, deck)
 
-        while not playable_hand:
-            deck = list()
-            for card in cards:
-                deck.append(tuple(card))        
+        playable_hand = do.ContainsType(hand, "Basic")
 
-            #shuffle(deck)
-            do.PrintCards("============\n=== DECK ===\n============", deck)
-    
-            for i in range(0,7):
-                do.DrawCard(hand, deck)
+        mulligans = mulligans + int(not playable_hand)
 
-            playable_hand = do.ContainsType(hand, "Basic")
+    if(mulligans in mulligans_by_number):
+        mulligans_by_number[mulligans] = mulligans_by_number[mulligans] + 1
+    else:
+        mulligans_by_number[mulligans] = 1
 
-            mulligans = mulligans + int(not playable_hand)
-
-        if(mulligans in mulligans_by_number):
-            mulligans_by_number[mulligans] = mulligans_by_number[mulligans] + 1
-        else:
-            mulligans_by_number[mulligans] = 1
-
-        do.PrintCards("====Actual Hand====", hand)
+    do.PrintCards("====Actual Hand====", hand)
 
 
-        # prize cards
-        prizes = list()
-        for i in range(0,6):
-            do.DrawCard(prizes, deck)
+    # prize cards
+    prizes = list()
+    for i in range(0,6):
+        do.DrawCard(prizes, deck)
 
-        do.PrintCards("====Prizes===", prizes)
-        disaster = False
-        for card in prizes:
-            # each card has a value...if all card of that value are in the prizes, that's a disaster
-            deck_indices = [i for i, x in enumerate(deck) if x[1] == card[1]]
-            prize_indices = [i for i, x in enumerate(prizes) if x[1] == card[1]]
+    do.PrintCards("====Prizes===", prizes)
+    disaster = False
+    for card in prizes:
+        # each card has a value...if all card of that value are in the prizes, that's a disaster
+        deck_indices = [i for i, x in enumerate(gold_deck) if x[1] == card[1]]
+        prize_indices = [i for i, x in enumerate(prizes) if x[1] == card[1]]
 
-            disaster = disaster | (len(deck_indices) == len(prize_indices))
+        disaster = disaster | (len(deck_indices) == len(prize_indices))
 
-        print "====Disaster: " + str(disaster)
-        disasters = disasters + disaster
+    print "====Disaster: " + str(disaster)
+    disasters = disasters + disaster
 
-        bench = []
+    bench = []
 
-        # play a Basic and draw a card
-        if(do.ContainsType(hand, "Basic")):
-            do.PlayBasic(hand, bench)
+    # play a Basic and draw a card
+    if(do.ContainsType(hand, "Basic")):
+        do.PlayBasic(hand, bench)
 
-        do.DrawCard(hand, deck)
+    do.DrawCard(hand, deck)
 
-        do.PrintCards("==========================New hand=", hand)
-        do.PrintCards("==========================Bench=", bench)
-        do.PrintCards("==========================Deck=", deck)
+    do.PrintCards("==========================New hand=", hand)
+    do.PrintCards("==========================Bench=", bench)
+    do.PrintCards("==========================Deck=", deck)
 
-        successes = successes + int(bft.BlastoiseFirstTurn(hand, discard, deck, bench, False, memoized))
+    successes = successes + int(bft.BlastoiseFirstTurn(hand, discard, deck, bench, False, memoized))
 
 print "================================="
 print "===Sims:      " + str(sims)
