53c30e1506c2c6e81059c745261ddbd4d45eb1ad
Benjamin Andken <benjamia@benjamia-netapp.local>
2015-03-07 07:03:26 -0800

    spit out stats as its running

--- PokeSim.py
+++ PokeSim.py
@@ -25,8 +25,9 @@ with open(filename, 'r+') as f:
         gold_deck.append(cf.create(card.rstrip()))
 
 
-for sim_number in range(0,sims):
-    print str(sim_number) + ":" + str(sims) + "::" + str(successes)
+for sim_number in range(1,sims+1):
+    print str(sim_number) + ":" + str(sims) + ":: successes: " + str(successes) + " (" + str(100.0*successes/sim_number) + "%) :: disasters: " + str(disasters) + " (" + str(100.0*disasters/sim_number) + "%) :: bad_news: " + str(bad_news) + " (" + str(100.0*bad_news/sim_number) + "%)"
+
     p = PlayerState()
 
     mulligans = 0
